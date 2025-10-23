2f0e00f072cc7addc2600824a6753b90a93444e4
Aaron Moura <moura.aaron06@gmail.com>
2025-10-23 20:18:46 -0300

    Define structures and function prototypes in lista.h

--- lista.h
+++ lista.h
@@ -1,1 +1,18 @@
+#ifndef lista
+#define lista
+#include <stdio.h>
 
+struct fila; //definir o TAD fila 
+struct paciente;
+
+
+struct fila InicHeap(fila);
+
+InicHeap
+void InsereHeap(int n int vetor[]);
+int ChecaHeap(int vetor[]);
+RemoveHeap
+void Heapfy(int vetor[])
+ImprimeHeap
+void HeapSort(int vetor[]); 
+AlteraHeap
