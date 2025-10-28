e990a63f44279287fa3200600ab02bec8d9d0144
Aaron Moura <moura.aaron06@gmail.com>
2025-10-28 16:59:34 -0300

    alterar comentários

--- main.c
+++ main.c
@@ -9,4 +9,5 @@
 * imprimir todos na sala de espera (imprimeheap)
 * ordenar todos pela sua ordem de prioridade
 * alterar a prioridade
+* fazer a comparação das maneiras de ordenar os vetores de 1024 elementos aleatórios
 */ 
