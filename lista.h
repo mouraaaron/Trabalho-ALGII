84193c7da0fbb5abc25e547140b02fbc768fd233
Aaron Moura <moura.aaron06@gmail.com>
2025-10-28 19:27:00 -0300

    Add function prototypes to lista.h

--- lista.h
+++ lista.h
@@ -4,12 +4,12 @@
 
 struct paciente;
 
-void InicHeap
-void InsereHeap
-int ChecaHeap
-void RemoveHeap
-void Heapfy
-void ImprimeHeap
-void HeapSort
-void AlteraHeap
+void InicHeap(paciente vetor[], int N);
+void InsereHeap(paciente vetor[], int N);
+int ChecaHeap(paciente vetor[], int N);
+void RemoveHeap(paciente Vetor[], int N);
+void Heapfy(paciente vetor[], int N);
+void ImprimeHeap(paciente Vetor[], int N);
+void HeapSort(paciente vetor[], int N);
+void AlteraHeap(paciente Vetor[], int N , int indice_paciente, int nova_prioridade);
 void SacodeHeap(paciente Vetor[], int N);
