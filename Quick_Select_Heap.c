adc6be23e8c7b6b4360be93ce400d90e72de68af
Aaron Moura <moura.aaron06@gmail.com>
2025-10-28 20:20:20 -0300

    Update Quick_Select_Heap.c

--- Quick_Select_Heap.c
+++ Quick_Select_Heap.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
-#include <string.>
-#include <stdlib.h>
-
-
-
-void InsereHeap(paciente Vetor[], int pos, paciente novo, int *comparacoes, int *trocas) {
-    Vetor[pos] = novo;
+void InsereHeapInt(int Vetor[], int pos, int valor, int *comparacoes, int *trocas) {
+    Vetor[pos] = valor;
     int i = pos;
-    paciente temp;
-    while (i > 1 && Vetor[i/2].prioridade < Vetor[i].prioridade) {
+    int temp;
+    while (i > 1 && Vetor[i/2] < Vetor[i]) {
         (*comparacoes)++;
         temp = Vetor[i/2];
         Vetor[i/2] = Vetor[i];
@@ -19,22 +13,22 @@ void InsereHeap(paciente Vetor[], int pos, paciente novo, int *comparacoes, int 
     if (i > 1) (*comparacoes)++; // última comparação que quebrou o while
 }
 
-void Heapfy(paciente Vetor[], int N, int *comparacoes, int *trocas) {
+void HeapfyInt(int Vetor[], int N, int *comparacoes, int *trocas) {
     for (int i = 2; i <= N; i++)
-        InsereHeap(Vetor, i, Vetor[i], comparacoes, trocas);
+        InsereHeapInt(Vetor, i, Vetor[i], comparacoes, trocas);
 }
 
-void SacodeHeap(paciente Vetor[], int N, int *comparacoes, int *trocas) {
+void SacodeHeapInt(int Vetor[], int N, int *comparacoes, int *trocas) {
     int i = 2;
-    paciente temp;
+    int temp;
     while (i <= N) {
         if (i < N) {
             (*comparacoes)++;
-            if (Vetor[i].prioridade < Vetor[i+1].prioridade)
+            if (Vetor[i] < Vetor[i+1])
                 i++;
         }
         (*comparacoes)++;
-        if (Vetor[i/2].prioridade >= Vetor[i].prioridade)
+        if (Vetor[i/2] >= Vetor[i])
             break;
         temp = Vetor[i/2];
         Vetor[i/2] = Vetor[i];
@@ -44,23 +38,21 @@ void SacodeHeap(paciente Vetor[], int N, int *comparacoes, int *trocas) {
     }
 }
 
-void HeapSort(paciente Vetor[], int N, int *comparacoes, int *trocas) {
-    paciente temp;
-    Heapfy(Vetor, N, comparacoes, trocas);
+void HeapSortInt(int Vetor[], int N, int *comparacoes, int *trocas) {
+    int temp;
+    HeapfyInt(Vetor, N, comparacoes, trocas);
     for (int i = N; i > 1; i--) {
         temp = Vetor[1];
         Vetor[1] = Vetor[i];
         Vetor[i] = temp;
         (*trocas)++;
-        SacodeHeap(Vetor, i-1, comparacoes, trocas);
+        SacodeHeapInt(Vetor, i-1, comparacoes, trocas);
     }
 }
 
-int ChecaHeap(paciente Vetor[], int N) {
-  for (int i = N; i > 1; i--)
-    if (Vetor[i].prioridade > Vetor[i/2].prioridade)
-      return 0;
-  return 1;
+int ChecaHeapInt(int Vetor[], int N) {
+    for (int i = N; i > 1; i--)
+        if (Vetor[i] > Vetor[i/2])
+            return 0;
+    return 1;
 }
-
-
