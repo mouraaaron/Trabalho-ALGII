986d9f8efb582dba1d0688dffbaf7f8a0c346c35
Lucas Sekikawa <lucassekikawa82@gmail.com>
2025-10-28 20:30:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,64 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "lista.h" 
 
-#define MAXPACIENTES 11
+#define MAXPACIENTES 10
+#define TAMANHO_VETOR 1024
+#define LIMITE_MAXIMO
+struct paciente leNovoPaciente ()
+{
+	struct paciente novoPaciente;
+        
+    printf("Digite o nome do paciente: ");
+    scanf("%s", novoPaciente.nome);
+
+    printf("Digite a prioridade do paciente: ");
+	scanf("%d", &novoPaciente.prioridade);
+    
+    return novoPaciente;
+}
+
+
 int main () {
 
-	int escolha;
+    int escolha;
     int contaPacientes = 0;
+	
     struct paciente FilaPacientes [MAXPACIENTES];
 
-	// Inicializa a fila de pacientes
-	inciaHeap(FilaPacientes);
-	
-	// Cria um looping que continua ate o botao de saida ser pressionado
-	do {
-	    puts("-------------- Menu de opcoes ---------------");
-	    puts(" 1 - Adicionar Paciente ");
-	    puts(" 2 - Chamar Proximo Paciente ");
-	    puts(" 3 - Imprimir Lista de Espera ");
-	    puts(" 4 - Ordenar Pacientes pela Prioridade " );
-	    puts(" 5 - Comparacaoo entre Quick e Selection Sort ");
-		puts(" 0 - Sair " );
-	    puts("---------------------------------------------");
-	    printf(" Digite o numero da operacao desejada: ");
-	    scanf("%d", &escolha);
-	
-	
-	    switch (escolha)
-	    {
-	        case 1:
-	            // Checa se e possivel adicionar mais pacientes
-	            if (contaPacientes < MAXPACIENTES)
-	            {
-	                InsereHeap(FilaPacientes, contaPacientes);
-	                contaPacientes++;
-	            }
-	                break;
-	        case 2:
-	            // Checa se existe paciente para ser removido
-	            if (contaPacientes != 0)
-	            {
-	                RemoveHeap(FilaPacientes, contaPacientes);
-	                contaPacientes--;
-	            }
-	            break;
-	        case 3:
-	                ImprimeHeap(FilaPacientes, contaPacientes);
-	                break;
-	        case 4:
-	                HeapSort(FilaPacientes, contaPacientes);
-	                break;
-	        case 5:
-	                printf("5");
-	                break;
-	    }
-		printf("\n");
-	} while (escolha != 0);
+    // Inicializa a fila de pacientes
+    inciaHeap(FilaPacientes);
 
-return 0;
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+	// Inicializa srand em 0 para padronizaçao dos resultados
+	srand(0);
+	
+    // Cria um looping que continua ate o botao de saida ser pressionado
+    do {
+        puts("\n-------------- Menu de opcoes ---------------"); // Adicionado '\n' para espaçamento
+        puts(" 1 - Adicionar Paciente");
+        puts(" 2 - Chamar Proximo Paciente");
+        puts(" 3 - Imprimir Lista de Espera");
+        puts(" 4 - Ordenar Pacientes pela Prioridade" );
+        puts(" 5 - Comparacao entre Heap, Quick e Selection Sort");
+        puts(" 0 - Sair" );
+        puts("---------------------------------------------");
+        printf(" Digite o numero da operacao desejada: ");
+        
+        scanf("%d", &escolha);
+    
+        switch (escolha)
+        {
+            case 1:
+                // Checa se e possivel adicionar mais pacientes
+                if (contaPacientes < MAXPACIENTES)
+                {
+                    struct paciente novoPaciente = leNovoPaciente();
+                    InsereHeap(FilaPacientes, contaPacientes, novoPaciente); 
+                    contaPacientes++;
+                } 
+				else 
+                    printf("ERRO: A fila de pacientes está cheia");
+                break;
+            case 2:
+                // Checa se existe paciente para ser removido
+                if (contaPacientes > 0)
+                {
+                    RemoveHeap(FilaPacientes, contaPacientes);
+                    contaPacientes--;
+                } 
+				else 
+                    printf("ERRO: Não há pacientes na fila de espera.\n");
+                break;
+            case 3:
+                puts("Lista de Espera:");
+                ImprimeHeap(FilaPacientes, contaPacientes);
+                break;
+            case 4:
+                HeapSort(FilaPacientes, contaPacientes);
+                break;
+            case 5:
+			{
+				int vetor_comparacao[TAMANHO_VETOR];
+				
+				for (int i=0; i<TAMANHO_VETOR; i++)
+					vetor_comparacao[i] = rand() % LIMITE_MAXIMO;
+
+				for (int i = 0; i < TAMANHO_COMPARACAO; i++) {
+        			vetor_quick[i] = vetor_original[i]; 
+        			vetor_selection[i] = vetor_original[i];
+				etor_heap[i] = vetor_original[i];
+    }
+				puts("Vetor de 1024 números aleatórios foi criado");
+			}	
+
+				
+                break;
+            case 0:
+                break;
+            default:
+                printf("Opção inválida (%d). Por favor, escolha de 0 a 5.\n", escolha);
+                break;
+        }
+    } while (escolha != 0);
+
+    return 0;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
                                                                                                                                                                                                                             42,45-66     Tudo
 
     
